ffcfb586fe9c74bf0869ddfaf5f9f465909970df
shababqcd <s.bazrafkan1@nuigalway.ie>
2018-09-05 12:32:33 +0100

    Update GASA_DCGAN_CFAR10_Attempt2_2.py

--- VACGANcifar10/GASA_DCGAN_CFAR10_Attempt2_2.py
+++ VACGANcifar10/GASA_DCGAN_CFAR10_Attempt2_2.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Example employing Lasagne for digit generation using the MNIST dataset and
-Deep Convolutional Generative Adversarial Networks
-(DCGANs, see http://arxiv.org/abs/1511.06434).
-It is based on the MNIST example in Lasagne:
-http://lasagne.readthedocs.org/en/latest/user/tutorial.html
-Note: In contrast to the original paper, this trains the generator and
-discriminator at once, not alternatingly. It's easy to change, though.
-Jan Schlüter, 2015-12-16
-"""
 
 from __future__ import print_function
 
@@ -100,57 +90,7 @@ def FirstTenSeedsGen(targets):
 
         SEEDt[ii,:] = seedT
     return SEEDt
-'''def load_dataset():
-    # We first define a download function, supporting both Python 2 and 3.
-    if sys.version_info[0] == 2:
-        from urllib import urlretrieve
-    else:
-        from urllib.request import urlretrieve
 
-    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
-        print("Downloading %s" % filename)
-        urlretrieve(source + filename, filename)
-
-    # We then define functions for loading MNIST images and labels.
-    # For convenience, they also download the requested files if needed.
-    import gzip
-
-    def load_mnist_images(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the inputs in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them to monochrome 2D images,
-        # following the shape convention: (examples, channels, rows, columns)
-        data = data.reshape(-1, 1, 28, 28)
-        # The inputs come as bytes, we convert them to float32 in range [0,1].
-        # (Actually to range [0, 255/256], for compatibility to the version
-        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
-        return data / np.float32(256)
-
-    def load_mnist_labels(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the labels in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
-        return data
-
-    # We can now download and read the training and test set images and labels.
-    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
-    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
-    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
-    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
-
-    # We reserve the last 10000 training examples for validation.
-    X_train, X_val = X_train[:-10000], X_train[-10000:]
-    y_train, y_val = y_train[:-10000], y_train[-10000:]
-
-    # We just return all the arrays in order, as expected in main().
-    # (It doesn't matter how we do this as long as we can read them again.)
-    return X_train, y_train, X_val, y_val, X_test, y_test'''
 
 def load_dataset():
     DATA = scipy.io.loadmat('data_batch_1.mat')
@@ -446,13 +386,6 @@ for epoch in range(num_epochs):
         progress = float(epoch) / num_epochs
         eta.set_value(lasagne.utils.floatX(initial_eta * 2 * (1 - progress)))
 
-# Optionally, you could now dump the network weights to a file like this:
-#np.savez('mnist_gen.npz', *lasagne.layers.get_all_param_values(generator))
-#np.savez('mnist_disc.npz', *lasagne.layers.get_all_param_values(discriminator))
-#
-# And load them again later on like this:
-# with np.load('model.npz') as f:
-#     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
-# lasagne.layers.set_all_param_values(network, param_values)
+
 
 
